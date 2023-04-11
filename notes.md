631defc8b4f9ed0d7b31899cddf898387dcbf26f
Benjamin Grayland <91722632+marmaladian@users.noreply.github.com>
2023-04-11 17:14:46 +1000

    Start CSR implementation

---
+++ notes.md
@@ -0,0 +1,2 @@
+# run commands
+- python3 spreadsheetFilebased.py csr sampleData.txt sampleCommands.in sample.out
--- spreadsheet/csrSpreadsheet.py
+++ spreadsheet/csrSpreadsheet.py
@@ -9,14 +9,12 @@ from spreadsheet.cell import Cell
 # __copyright__ = 'Copyright 2023, RMIT University'
 # ------------------------------------------------------------------------
 
-
-
-
 class CSRSpreadsheet(BaseSpreadsheet):
 
     def __init__(self):
-        # TO BE IMPLEMENTED
-        pass
+        self.cola = []      # indicates which columns have values
+        self.vala = []      # indicates the values for populated cells
+        self.suma = []      # indicates the cumulative sum up to [n]th row
 
 
     def buildSpreadsheet(self, lCells: [Cell]):
@@ -25,8 +23,15 @@ class CSRSpreadsheet(BaseSpreadsheet):
         @param lCells: list of cells to be stored
         """
 
-        # TO BE IMPLEMENTED
-        pass
+        for cell in lCells:
+            success = self.update(cell.row, cell.col, cell.val)
+            if not success:
+                # well, we gotta add the row or column.
+
+                pass
+        print(self.cola)
+        print(self.vala)
+        print(self.suma)
 
 
     def appendRow(self):
@@ -88,29 +93,54 @@ class CSRSpreadsheet(BaseSpreadsheet):
 
         @return True if cell can be updated.  False if cannot, e.g., row or column indices do not exist.
         """
+        can_update = True
 
-        # TO BE IMPLEMENTED
+        if rowIndex >= len(self.suma):
+            can_update = False
+            print('row out of bounds')
+        else:
+            row_start_sum = self.suma[rowIndex] # if row > current, then return false.
+            row_end_sum = self.suma[rowIndex + 1]
+            cola_index, cur_sum = 0
+            while cur_sum < row_start_sum:
+                cur_sum += self.val[cola_index]
+                cola_index += 1
+            # now we are in the right part of col_a
+            # just have to check if col_a has an entry of our col value, or not
+            # before we go past the end of the row (the corresponding valas exceed
+            # suma[rowIndex+1])
+            # also need to consider if we're at the last row... need to add new row.
+            while (self.cola[cola_index] != colIndex) and (cur_sum < row_end_sum):
+                cur_sum += self.val[cola_index]
+                cola_index += 1
+                if cur_sum >= row_end_sum:
+                    self.cola.insert(cola_index - 1, colIndex)
+                    self.vala.insert(cola_index - 1, value)
+                    # update the rows
+                    for row in range(rowIndex + 1, len(self.suma)):
+                        self.suma[row] += value
 
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        return True
+                # if the value at the new cola index tips the sum over, then the value
+                # needs to go in just before it.
+
+        return can_update
 
 
     def rowNum(self)->int:
         """
         @return Number of rows the spreadsheet has.
         """
-        # TO BE IMPLEMENTED
-        return 0
+        return len(self.suma) - 1
 
 
     def colNum(self)->int:
         """
-        @return Number of column the spreadsheet has.
+        @return Number of columns the spreadsheet has.
         """
-        # TO BE IMPLEMENTED
-        return 0
-
-
+        # HACK  could just store highest col seen as property of spreadsheet
+        #       and update when adding/removing?
+        # think this will return 0 if cola is null.
+        return max(self.cola or [0])
 
 
     def find(self, value: float) -> [(int, int)]:
@@ -126,8 +156,6 @@ class CSRSpreadsheet(BaseSpreadsheet):
 
         # REPLACE WITH APPROPRIATE RETURN VALUE
         return []
-
-
 
 
     def entries(self) -> [Cell]:
