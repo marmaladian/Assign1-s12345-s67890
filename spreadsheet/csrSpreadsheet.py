996e8f70241fe1a91d033a56d983ac65601f6c91
Benjamin Grayland <91722632+marmaladian@users.noreply.github.com>
2023-04-13 14:49:42 +1000

    Add print spreadsheet method

--- spreadsheet/csrSpreadsheet.py
+++ spreadsheet/csrSpreadsheet.py
@@ -1,5 +1,6 @@
 from spreadsheet.baseSpreadsheet import BaseSpreadsheet
 from spreadsheet.cell import Cell
+from typing import List, Tuple
 
 # ------------------------------------------------------------------------
 # This class is required TO BE IMPLEMENTED
@@ -14,25 +15,31 @@ class CSRSpreadsheet(BaseSpreadsheet):
     def __init__(self):
         self.cola = []      # indicates which columns have values
         self.vala = []      # indicates the values for populated cells
-        self.suma = []      # indicates the cumulative sum up to [n]th row
+        self.suma = []      # indicates the cumulative sum up to [n]th row, len = num_rows + 1
+        self.num_cols = 0   # needed when empty columns are appended
 
 
-    def buildSpreadsheet(self, lCells: [Cell]):
+    def buildSpreadsheet(self, lCells: List[Cell]):
         """
         Construct the data structure to store nodes.
         @param lCells: list of cells to be stored
         """
-
+        print("BUILDING SPREADSHEET")
         for cell in lCells:
-            success = self.update(cell.row, cell.col, cell.val)
-            if not success:
+            print(f"trying to add {cell.val} at {cell.row}, {cell.col}")
+            while not self.update(cell.row, cell.col, cell.val):
                 # well, we gotta add the row or column.
-
-                pass
-        print(self.cola)
-        print(self.vala)
-        print(self.suma)
-
+                if cell.row >= self.num_rows():
+                    self.appendRow()
+                if cell.col >= self.num_cols:
+                    self.appendCol()
+        
+        # HACK
+        # self.suma = [0, 5, 6, 6]
+        # self.cola = [0, 1, 2, 3]
+        # self.vala = [5, -2, 2, 1]
+        # self.num_cols = 4
+        self.print_spreadsheet()
 
     def appendRow(self):
         """
@@ -40,10 +47,12 @@ class CSRSpreadsheet(BaseSpreadsheet):
 
         @return True if operation was successful, or False if not.
         """
-
-        # TO BE IMPLEMENTED
-        pass
-
+        if not self.suma:
+            self.suma.append(0)
+        sum_for_new_row = self.suma[-1] if self.suma else 0
+        self.suma.append(sum_for_new_row)
+        print(f"append row: new size is R {self.num_rows()}, C {self.num_cols}")
+        return True         # why would this fail? too many rows?
 
     def appendCol(self):
         """
@@ -51,36 +60,42 @@ class CSRSpreadsheet(BaseSpreadsheet):
 
         @return True if operation was successful, or False if not.
         """
-
-        # TO BE IMPLEMENTED
-        pass
+        self.num_cols += 1
+        print(f"append col: new size is R {self.num_rows()}, C {self.num_cols}")
+        return True         # why would this fail?
 
 
     def insertRow(self, rowIndex: int)->bool:
         """
         Inserts an empty row into the spreadsheet.
 
-        @param rowIndex Index of the existing row that will be after the newly inserted row.  If inserting as first row, specify rowIndex to be 0.  If inserting a row after the last one, specify rowIndex to be rowNum()-1.
+        @param rowIndex Index of the existing row to insert the new row AFTER.  If inserting as first row, specify rowIndex to be -1.
 
         @return True if operation was successful, or False if not, e.g., rowIndex is invalid.
         """
-
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        return True
+        success = False
+        if -1 <= rowIndex < self.num_rows():            # check range
+            sum_at_row = self.suma[rowIndex + 1]        # sum for new row = sum for row after
+            self.suma.insert(rowIndex + 1, sum_at_row)  # python insert is BEFORE index
+            success = True
+        return success
 
 
     def insertCol(self, colIndex: int)->bool:
         """
         Inserts an empty column into the spreadsheet.
 
-        @param colIndex Index of the existing column that will be after the newly inserted row.  If inserting as first column, specify colIndex to be 0.  If inserting a column after the last one, specify colIndex to be colNum()-1.
+        @param colIndex Index of the existing column to insert the new column  AFTER. If inserting as first row, specify colIndex to be -1.
 
         return True if operation was successful, or False if not, e.g., colIndex is invalid.
         """
-
-        # REPLACE WITH APPROPRIATE RETURN VALUE
-        return True
-
+        success = False
+        if -1 <= colIndex < self.num_cols:
+            for col in self.cola:
+                if col > colIndex:
+                    col += 1
+            success = True
+        return success
 
 
     def update(self, rowIndex: int, colIndex: int, value: float) -> bool:
@@ -95,15 +110,15 @@ class CSRSpreadsheet(BaseSpreadsheet):
         """
         can_update = True
 
-        if rowIndex >= len(self.suma):
+        if (rowIndex >= self.num_rows()) or (colIndex >= self.num_cols):
             can_update = False
-            print('row out of bounds')
+            print('row or col out of bounds')
         else:
             row_start_sum = self.suma[rowIndex] # if row > current, then return false.
             row_end_sum = self.suma[rowIndex + 1]
-            cola_index, cur_sum = 0
+            cola_index, cur_sum = 0, 0
             while cur_sum < row_start_sum:
-                cur_sum += self.val[cola_index]
+                cur_sum += self.vala[cola_index]
                 cola_index += 1
             # now we are in the right part of col_a
             # just have to check if col_a has an entry of our col value, or not
@@ -111,7 +126,7 @@ class CSRSpreadsheet(BaseSpreadsheet):
             # suma[rowIndex+1])
             # also need to consider if we're at the last row... need to add new row.
             while (self.cola[cola_index] != colIndex) and (cur_sum < row_end_sum):
-                cur_sum += self.val[cola_index]
+                cur_sum += self.vala[cola_index]
                 cola_index += 1
                 if cur_sum >= row_end_sum:
                     self.cola.insert(cola_index - 1, colIndex)
@@ -130,20 +145,17 @@ class CSRSpreadsheet(BaseSpreadsheet):
         """
         @return Number of rows the spreadsheet has.
         """
-        return len(self.suma) - 1
+        return self.num_rows()
 
 
     def colNum(self)->int:
         """
         @return Number of columns the spreadsheet has.
         """
-        # HACK  could just store highest col seen as property of spreadsheet
-        #       and update when adding/removing?
-        # think this will return 0 if cola is null.
-        return max(self.cola or [0])
+        return self.num_cols
 
 
-    def find(self, value: float) -> [(int, int)]:
+    def find(self, value: float) -> List[Tuple[int, int]]:
         """
         Find and return a list of cells that contain the value 'value'.
 
@@ -158,9 +170,46 @@ class CSRSpreadsheet(BaseSpreadsheet):
         return []
 
 
-    def entries(self) -> [Cell]:
+    def entries(self) -> List[Cell]:
         """
-        return a list of cells that have values (i.e., all non None cells).
+        @return A list of cells that have values (i.e., all non None cells).
         """
-
         return []
+
+
+    def num_rows(self) -> int:
+        """
+        @return Number of rows in the spreadsheet.
+        """
+        return len(self.suma) - 1 if self.suma else 0
+    
+
+    def print_spreadsheet(self) -> None:
+        """
+        Prints the spreadsheet to the terminal.
+        """
+        sum = 0
+        vc_index = 0
+        for row_sum in self.suma:
+            col = 0
+            while sum < row_sum:
+                if col == self.cola[vc_index]:
+                    value = self.vala[vc_index]
+                    print(value, end='\t')
+                    sum += value
+                    vc_index += 1
+                else:
+                    print('-', end='\t')
+                col += 1
+            while col < self.num_cols:
+                print('-', end='\t')
+                col += 1
+            print()
+        print('---')
+        print(self.cola)
+        print(self.vala)
+        print(self.suma)
+
+            
+
+
