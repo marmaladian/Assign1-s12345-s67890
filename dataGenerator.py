bc91d419ed44b097b2d616abc0455179fbd488d1
GeordieEK <geordieek@gmail.com>
2023-04-14 14:27:34 +1000

    dataGenerator can now be run from CLI number of rows and cols can be specified separately high and low vals can be specified separately output file generated from input parameters

--- dataGenerator.py
+++ dataGenerator.py
@@ -1,36 +1,44 @@
 import random
+import sys
 
 if __name__ == '__main__':
     '''
     dataGen generates a text file of spreadsheet values
+    filename is in the format: numRows_numCols_filled_lowVal_highVal_distribution
+    run from the command line with the following arguments:
 
-    @param size: the size of the spreadsheet
-    @param filled: the percentage of the spreadsheet that is filled
-    @param valRange: the range of values in the spreadsheet
+    @param numCols: the number of columns
+    @param numRows: the number of rows
+    @param filled: fill probability (0.0 - 1.0)
+    @param lowVal: the lowest possible value in the spreadsheet
+    @param highVal: the highest possible value in the spreadsheet
     @param distribution: the distribution of the values in the spreadsheet (uniform, normal)
     '''
-    def dataGen(size, filled, valRange, distribution):
+    # Fetch the command line arguments
+    args = sys.argv
+
+    def dataGen(numRows, numCols, filled, lowVal, highVal, distribution):
         # Create a file for writing
-        with open('sampleSpreadsheet.txt', 'x') as file:
-            # Calculate the number of cells to fill
-            numCells = int(size * size * filled)
+        with open(str(numRows) + '_' + str(numCols) + '_' + str(filled) + '_' + str(lowVal) + '_' + str(highVal) + '_' + distribution, 'w') as file:
 
             # Loop through each cell in the spreadsheet
-            for row in range(numCells):
-                for col in range(numCells):
+            for row in range(numRows):
+                for col in range(numCols):
                     # Fill probability
                     if random.random() < filled:
                         # Generate a value for this cell based on the distribution
                         if distribution == 'uniform':
-                            value = round(random.uniform(-valRange, valRange),
+                            value = round(random.uniform(lowVal, highVal),
                                           random.randint(0, 5))
                         elif distribution == 'normal':
                             value = round(random.normalvariate(
-                                -valRange, valRange), random.randint(0, 5))
+                                lowVal, highVal), random.randint(0, 5))
                         else:
                             raise ValueError('Invalid distribution')
 
                         # Write the row, column, and value to the file
                         file.write(f'{row} {col} {value}\n')
 
-    dataGen(10, 0.1, 100, 'uniform')
+    # call the function:
+    dataGen(int(args[1]), int(args[2]), float(args[3]),
+            int(args[4]), int(args[5]), args[6])
