17a8feaa07638802dfc0dbbc3c2963b1ed115b4a
Benjamin Grayland <91722632+marmaladian@users.noreply.github.com>
2023-04-15 22:41:24 +1000

    Complete tessting framework

--- generation/dataGenerator.py
+++ generation/dataGenerator.py
@@ -1,36 +1,37 @@
 import random
 import sys
 
-if __name__ == '__main__':
-    '''
-    dataGen generates a text file of spreadsheet values
-    filename is in the format: numRows_numCols_filled_lowVal_highVal_distribution
-    run from the command line with the following arguments:
+'''
+dataGen generates a text file of spreadsheet values
+filename is in the format: numRows_numCols_filled_lowVal_highVal_distribution
+run from the command line with the following arguments:
 
-    @param numRows: the number of rows
-    @param numCols: the number of columns
-    @param filled: fill probability (0.0 - 1.0)
-    @param lowVal: the lowest possible value in the spreadsheet
-    @param highVal: the highest possible value in the spreadsheet
-    '''
-    # Fetch the command line arguments
-    args = sys.argv
+@param numRows: the number of rows
+@param numCols: the number of columns
+@param filled: fill probability (0.0 - 1.0)
+@param lowVal: the lowest possible value in the spreadsheet
+@param highVal: the highest possible value in the spreadsheet
+'''
+# Fetch the command line arguments
+args = sys.argv
 
-    def dataGen(numRows, numCols, filled, lowVal, highVal):
-        # Create a file for writing
-        with open(str(numRows) + '_' + str(numCols) + '_' + str(filled) + '_' + str(lowVal) + '_' + str(highVal), 'w') as file:
+def dataGen(directory, numRows, numCols, filled, lowVal, highVal):
+    # Create a file for writing
+    with open(directory + '/' + str(numRows) + '_' + str(numCols) + '_' + str(filled) + '_' + str(lowVal) + '_' + str(highVal), 'w') as file:
 
-            # Loop through each cell in the spreadsheet
-            for row in range(numRows):
-                for col in range(numCols):
-                    # Fill probability
-                    if random.random() < filled:
-                        # Generate a randomised value
-                        value = round(random.uniform(lowVal, highVal),
-                                      random.randint(0, 5))
+        # Loop through each cell in the spreadsheet
+        for row in range(numRows):
+            for col in range(numCols):
+                # Fill probability
+                if random.random() < filled:
+                    # Generate a randomised value
+                    value = round(random.uniform(lowVal, highVal),
+                                    random.randint(0, 5))
 
-                        # Write the row, column, and value to the file
+                    # Write the row, column, and value to the file
                     file.write(f'{row} {col} {value}\n')
+
+if __name__ == '__main__':
 
     # call the function:
     dataGen(int(args[1]), int(args[2]), float(args[3]),
--- testing.py
+++ testing.py
@@ -5,8 +5,9 @@ from spreadsheet.cell import Cell
 from spreadsheet.arraySpreadsheet import ArraySpreadsheet
 from spreadsheet.linkedlistSpreadsheet import LinkedListSpreadsheet
 from spreadsheet.csrSpreadsheet import CSRSpreadsheet
+from generation import dataGenerator
 
-data_dir = 'generation'
+data_dir = 'data_files'
 data_file_extension = '.data'
 
 if __name__ == '__main__':
@@ -21,14 +22,20 @@ if __name__ == '__main__':
 
     data_files = []
     test_cases = []
+    results = []
+
+    def remove_data_files():
+        print('Removing old data files!')
+        for file in os.listdir(data_dir):
+            os.remove(os.path.join(data_dir, file))
+    
 
     def get_data_files():
         success = True
         print('Looking for data files:')
         for file in os.listdir(data_dir):
-            if file.endswith(data_file_extension):
-                data_files.append(file)
-                print('\t', os.path.join(data_dir, file))
+            data_files.append(file)
+            print('\t', os.path.join(data_dir, file))
         if not data_files:
             print('No data files found!')
             success = False
@@ -83,28 +90,44 @@ if __name__ == '__main__':
             linked_list = test_case['linked_list']
             csr = test_case['csr']
 
-            (_, _, _, _, max_val, _) = test_case['filename'].split('_')
+            print(test_case['filename'].split('_'))
+
+            (rows, cols, fill_percent, min_val, max_val) = test_case['filename'].split('_')
             findable_value = random.choice(test_case['values'])
             not_findable_value = float(max_val) + 25
 
             def find_test_helper(spreadsheet, value):
                 result = spreadsheet.find(value)
 
-            print(f'testing findable values for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: find_test_helper(array, findable_value), number=iterations))
-            print('ll:\t', timeit.timeit(lambda: find_test_helper(linked_list, findable_value), number=iterations))
-            print('csr:\t', timeit.timeit(lambda: find_test_helper(csr, findable_value), number=iterations))
-            print()
+            data_desc = f'R {rows}, C {cols}, ~{fill_percent} filled, {iterations} iterations'
 
-            print(f'testing unfindable values for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: find_test_helper(array, not_findable_value), number=iterations))
-            print('ll:\t', timeit.timeit(lambda: find_test_helper(linked_list, not_findable_value), number=iterations))
-            print('csr:\t', timeit.timeit(lambda: find_test_helper(csr, not_findable_value), number=iterations))
-            print()
+            tests = [
+                ['find: existing value',        data_desc, lambda: findable_value],
+                ['find: non-existing value',    data_desc, lambda: not_findable_value]
+            ]
+
+            for test in tests:
+                print('executing: ', test[0])
+                results.append([test[0], test[1], 'array', timeit.timeit(lambda: find_test_helper(array, test[2]()), number=iterations)])
+                results.append([test[0], test[1], 'linked_list', timeit.timeit(lambda: find_test_helper(linked_list, test[2]()), number=iterations)])
+                results.append([test[0], test[1], 'csr', timeit.timeit(lambda: find_test_helper(csr, test[2]()), number=iterations)])
+
+
+            # print(f'testing findable values for {test_case["filename"]}')
+            # print('array:\t', timeit.timeit(lambda: find_test_helper(array, findable_value), number=iterations))
+            # print('ll:\t', timeit.timeit(lambda: find_test_helper(linked_list, findable_value), number=iterations))
+            # print('csr:\t', timeit.timeit(lambda: find_test_helper(csr, findable_value), number=iterations))
+            # print()
+
+            # print(f'testing unfindable values for {test_case["filename"]}')
+            # print('array:\t', timeit.timeit(lambda: find_test_helper(array, not_findable_value), number=iterations))
+            # print('ll:\t', timeit.timeit(lambda: find_test_helper(linked_list, not_findable_value), number=iterations))
+            # print('csr:\t', timeit.timeit(lambda: find_test_helper(csr, not_findable_value), number=iterations))
+            # print()
 
     def test_insert(iterations):
         for test_case in test_cases:
-            (rows, cols, fill_percent, min_val, max_val, _) = test_case['filename'].split('_')
+            (rows, cols, fill_percent, min_val, max_val) = test_case['filename'].split('_')
 
             array = test_case['array']
             linked_list = test_case['linked_list']
@@ -119,82 +142,40 @@ if __name__ == '__main__':
             def insert_col_test_helper(spreadsheet, col):
                 spreadsheet.insertCol(col)
 
-            # ROWS
-            # insert row at start
-            print(f'testing insert row at start for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: insert_row_test_helper(array, 0), number=iterations))
-            print('ll:\t', timeit.timeit(lambda: insert_row_test_helper(linked_list, 0), number=iterations))
-            print('csr:\t', timeit.timeit(lambda: insert_row_test_helper(csr, 0), number=iterations))
-            print()
+            data_desc = f'R {rows}, C {cols}, ~{fill_percent} filled, {iterations} iterations'
 
-            # insert row at end
-            print(f'testing insert at last row for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: insert_row_test_helper(array, last_row), number=iterations))
-            print('ll:\t', timeit.timeit(lambda: insert_row_test_helper(linked_list, last_row), number=iterations))
-            print('csr:\t', timeit.timeit(lambda: insert_row_test_helper(csr, last_row), number=iterations))
-            print()
+            row_tests = [
+                ['insert: row at start',        data_desc, lambda: 0],
+                ['insert: row at end',          data_desc, lambda: last_row],
+                ['insert: row after end',       data_desc, lambda: -1],
+                ['insert: row into middle',     data_desc, lambda: last_row // 2],
+                ['insert: row at random pos.',  data_desc, lambda: random.randint(0, last_row)]
+            ]
 
-            # append row
-            print(f'testing insert row at end (append) for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: insert_row_test_helper(array, -1), number=iterations))
-            print('ll:\t', timeit.timeit(lambda: insert_row_test_helper(linked_list, -1), number=iterations))
-            print('csr:\t', timeit.timeit(lambda: insert_row_test_helper(csr, -1), number=iterations))
-            print()
-            
-            # insert row in middle
-            print(f'testing insert at middle-ish row for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: insert_row_test_helper(array, last_row // 2), number=iterations))
-            print('ll:\t', timeit.timeit(lambda: insert_row_test_helper(linked_list, last_row // 2), number=iterations))
-            print('csr:\t', timeit.timeit(lambda: insert_row_test_helper(csr, last_row // 2), number=iterations))
-            print()
+            col_tests = [
+                ['insert: col at start',        data_desc, lambda: 0],
+                ['insert: col at end',          data_desc, lambda: last_col],
+                ['insert: col after end',       data_desc, lambda: -1],
+                ['insert: col into middle',     data_desc, lambda: last_col // 2],
+                ['insert: col at random pos.',  data_desc, lambda: random.randint(0, last_col)]
+            ]
 
-            # insert random row
-            print(f'testing insert at random row for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: insert_row_test_helper(array, random.randint(0, last_row)), number=iterations))
-            print('ll:\t', timeit.timeit(lambda: insert_row_test_helper(linked_list, random.randint(0, last_row)), number=iterations))
-            print('csr:\t', timeit.timeit(lambda: insert_row_test_helper(csr, random.randint(0, last_row)), number=iterations))
-            print()
+            for test in row_tests:
+                print('executing: ', test[0])
+                results.append([test[0], test[1], 'array', timeit.timeit(lambda: insert_row_test_helper(array, test[2]()), number=iterations)])
+                results.append([test[0], test[1], 'linked_list', timeit.timeit(lambda: insert_row_test_helper(linked_list, test[2]()), number=iterations)])
+                results.append([test[0], test[1], 'csr', timeit.timeit(lambda: insert_row_test_helper(csr, test[2]()), number=iterations)])
 
-            # COLS
-            # insert col at start
-            print(f'testing insert col at start for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: insert_col_test_helper(array, 0), number=iterations))
-            print('ll:\t', timeit.timeit(lambda: insert_col_test_helper(linked_list, 0), number=iterations))
-            print('csr:\t', timeit.timeit(lambda: insert_col_test_helper(csr, 0), number=iterations))
-            print()
-
-            # insert col at end
-            print(f'testing insert at last col for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: insert_col_test_helper(array, last_col), number=iterations))
-            print('ll:\t', timeit.timeit(lambda: insert_col_test_helper(linked_list, last_col), number=iterations))
-            print('csr:\t', timeit.timeit(lambda: insert_col_test_helper(csr, last_col), number=iterations))
-            print()
-
-            # append col
-            print(f'testing insert col at end (append) for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: insert_col_test_helper(array, -1), number=iterations))
-            print('ll:\t', timeit.timeit(lambda: insert_col_test_helper(linked_list, -1), number=iterations))
-            print('csr:\t', timeit.timeit(lambda: insert_col_test_helper(csr, -1), number=iterations))
-            print()
-            
-            # insert col in middle
-            print(f'testing insert at middle-ish col for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: insert_col_test_helper(array, last_col // 2), number=iterations))
-            print('ll:\t', timeit.timeit(lambda: insert_col_test_helper(linked_list, last_col // 2), number=iterations))
-            print('csr:\t', timeit.timeit(lambda: insert_col_test_helper(csr, last_col // 2), number=iterations))
-            print()
-
-            # insert random col
-            print(f'testing insert at random col for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: insert_col_test_helper(array, random.randint(0, last_col)), number=iterations))
-            print('ll:\t', timeit.timeit(lambda: insert_col_test_helper(linked_list, random.randint(0, last_col)), number=iterations))
-            print('csr:\t', timeit.timeit(lambda: insert_col_test_helper(csr, random.randint(0, last_col)), number=iterations))
-            print()
+            for test in col_tests:
+                print('executing: ', test[0])
+                results.append([test[0], test[1], 'array', timeit.timeit(lambda: insert_col_test_helper(array, test[2]()), number=iterations)])
+                results.append([test[0], test[1], 'linked_list', timeit.timeit(lambda: insert_col_test_helper(linked_list, test[2]()), number=iterations)])
+                results.append([test[0], test[1], 'csr', timeit.timeit(lambda: insert_col_test_helper(csr, test[2]()), number=iterations)])
 
 
     def test_update(iterations):
         for test_case in test_cases:
-            (rows, cols, fill_percent, min_val, max_val, _) = test_case['filename'].split('_')
+            (rows, cols, fill_percent, min_val, max_val) = test_case['filename'].split('_')
 
             array = test_case['array']
             linked_list = test_case['linked_list']
@@ -206,44 +187,66 @@ if __name__ == '__main__':
 
             def update_test_helper(spreadsheet, row, col, value):
                 spreadsheet.update(row, col, value)
-                # result = spreadsheet.update(row, col, value)
-                # if not result:
-                    # raise RuntimeError('Update should not fail in testing')
 
+            data_desc = f'R {rows}, C {cols}, ~{fill_percent} filled, {iterations} iterations'
 
-            # test random row, col
-            print(f'testing random row, col update for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: update_test_helper(array, random.randint(0, last_row), random.randint(0, last_col), update_value), number=iterations))
-            print('ll:\t',    timeit.timeit(lambda: update_test_helper(array, random.randint(0, last_row), random.randint(0, last_col), update_value), number=iterations))
-            print('csr:\t',   timeit.timeit(lambda: update_test_helper(array, random.randint(0, last_row), random.randint(0, last_col), update_value), number=iterations))
-            print()
+            tests = [
+                ['update: random row, random column', data_desc, lambda: random.randint(0, last_row), lambda: random.randint(0, last_col), update_value],
+                ['update: first row, first column', data_desc, lambda: 0, lambda: 0, update_value],
+                ['update: last row, last column', data_desc, lambda: last_row, lambda: last_col, update_value]
+            ]
 
-            # test first row, first col
-            print(f'testing first row, first col update for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: update_test_helper(array, 0, 0, update_value), number=iterations))
-            print('ll:\t',    timeit.timeit(lambda: update_test_helper(array, 0, 0, update_value), number=iterations))
-            print('csr:\t',   timeit.timeit(lambda: update_test_helper(array, 0, 0, update_value), number=iterations))
-            print()
-
-            # test last row, last col
-            print(f'testing last row, last col update for {test_case["filename"]}')
-            print('array:\t', timeit.timeit(lambda: update_test_helper(array, last_row, last_col, update_value), number=iterations))
-            print('ll:\t',    timeit.timeit(lambda: update_test_helper(array, last_row, last_col, update_value), number=iterations))
-            print('csr:\t',   timeit.timeit(lambda: update_test_helper(array, last_row, last_col, update_value), number=iterations))
-            print()
-
+            for test in tests:
+                print('executing: ', test[0])
+                results.append([test[0], test[1], 'array', timeit.timeit(lambda: update_test_helper(array, test[2](), test[3](), update_value), number=iterations)])
+                results.append([test[0], test[1], 'linked_list', timeit.timeit(lambda: update_test_helper(linked_list, test[2](), test[3](), update_value), number=iterations)])
+                results.append([test[0], test[1], 'csr', timeit.timeit(lambda: update_test_helper(csr, test[2](), test[3](), update_value), number=iterations)])
 
     def run():
+        # delete existing files in data directory
+        remove_data_files()
+        
+        print('Generating new data files...')
+        # create source data
+        # small spreadsheets
+        small = 50
+        dataGenerator.dataGen(data_dir, small, small,               0.3, -100000, 100000)
+        dataGenerator.dataGen(data_dir, small // 10, small * 10,    0.3, -100000, 100000)
+        dataGenerator.dataGen(data_dir, small * 10, small // 10,    0.3, -100000, 100000)
+        
+        dataGenerator.dataGen(data_dir, small, small,               1.0, -100000, 100000)
+        dataGenerator.dataGen(data_dir, small // 10, small * 10,    1.0, -100000, 100000)
+        dataGenerator.dataGen(data_dir, small * 10, small // 10,    1.0, -100000, 100000)
+
+        # medium spreadsheets
+        medium = 250
+        dataGenerator.dataGen(data_dir, medium, medium,             0.3, -100000, 100000)
+        dataGenerator.dataGen(data_dir, medium // 10, medium * 10,  0.3, -100000, 100000)
+        dataGenerator.dataGen(data_dir, medium * 10, medium // 10,  0.3, -100000, 100000)
+        
+        dataGenerator.dataGen(data_dir, medium, medium,             1.0, -100000, 100000)
+        dataGenerator.dataGen(data_dir, medium // 10, medium * 10,  1.0, -100000, 100000)
+        dataGenerator.dataGen(data_dir, medium * 10, medium // 10,  1.0, -100000, 100000)
+
+        # large spreadsheets
+        large = 500
+        dataGenerator.dataGen(data_dir, large, large,               0.3, -100000, 100000)
+        dataGenerator.dataGen(data_dir, large // 10, large * 10,    0.3, -100000, 100000)
+        dataGenerator.dataGen(data_dir, large * 10, large // 10,    0.3, -100000, 100000)
+        
+        dataGenerator.dataGen(data_dir, large, large,               1.0, -100000, 100000)
+        dataGenerator.dataGen(data_dir, large // 10, large * 10,    1.0, -100000, 100000)
+        dataGenerator.dataGen(data_dir, large * 10, large // 10,    1.0, -100000, 100000)
+
         if (get_data_files()):
             create_spreadsheets()
-            # test_find(100)
-            test_insert(100)
-            # test_update(10000)
+            test_find(10)
+            test_insert(10)
+            test_update(10)
 
-
-        
-
-
-
+        print(f'{len(results)} tests completed.')
+        print('---------------------------------------------------------------------------------------------------------------------------------')
+        for result in results:
+            print(f'{result[0]:40}\t{result[1]:30}\t{result[2]:10}\t{result[3]}')
 
     run()
